cf9876167ae34b4d45a3facab2bdb206a1b5d328
Kamran Nasirov <13778@holbertonstudents.com>
2026-04-11 13:45:19 +0000

    6-square.py

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,74 +1,65 @@
 #!/usr/bin/python3
-"""Singly linked list üçün Node və LinkedList klassları."""
+"""Kvadratı təyin edən Square sinfi modulu."""
 
 
-class Node:
-    """Linked list-in düyününü (Node) təmsil edən klass."""
+class Square:
+    """Kvadratı təmsil edən sinif."""
 
-    def __init__(self, data, next_node=None):
-        """Node-u inisializasiya edir."""
-        self.data = data
-        self.next_node = next_node
+    def __init__(self, size=0, position=(0, 0)):
+        """Yeni Square obyektini inisializasiya edir.
 
-    @property
-    def data(self):
-        """Data getter-i."""
-        return self.__data
-
-    @data.setter
-    def data(self, value):
-        """Data setter-i (mütləq tam ədəd olmalıdır)."""
-        if not isinstance(value, int):
-            raise TypeError("data must be an integer")
-        self.__data = value
-
-    @property
-    def next_node(self):
-        """Next_node getter-i."""
-        return self.__next_node
-
-    @next_node.setter
-    def next_node(self, value):
-        """Next_node setter-i (Node obyekti və ya None olmalıdır)."""
-        if not isinstance(value, Node) and value is not None:
-            raise TypeError("next_node must be a Node object")
-        self.__next_node = value
-
-
-class SinglyLinkedList:
-    """Singly linked list-i idarə edən klass."""
-
-    def __init__(self):
-        """Boş bir list yaradır."""
-        self.__head = None
-
-    def __str__(self):
-        """List-i çap etmək üçün mətni formalaşdırır."""
-        values = []
-        tmp = self.__head
-        while tmp is not None:
-            values.append(str(tmp.data))
-            tmp = tmp.next_node
-        return "\n".join(values)
-
-    def sorted_insert(self, value):
-        """
-        Yeni Node-u listin daxilinə artan sıra ilə əlavə edir.
-        
         Args:
-            value (int): Əlavə olunacaq rəqəm.
+            size (int): Kvadratın tərəfinin ölçüsü.
+            position (int, int): Kvadratın koordinatları (tuple).
         """
-        new_node = Node(value)
-        
-        # Əgər list boşdursa və ya yeni dəyər başdakından kiçikdirsə
-        if self.__head is None or self.__head.data >= value:
-            new_node.next_node = self.__head
-            self.__head = new_node
-        else:
-            # Düzgün mövqeyi tapmaq üçün listi gəzirik
-            tmp = self.__head
-            while tmp.next_node is not None and tmp.next_node.data < value:
-                tmp = tmp.next_node
-            
-            new_node.next_node = tmp.next_node
-            tmp.next_node = new_node
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Kvadratın ölçüsünü geri qaytarır."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Kvadratın ölçüsünü təyin edir və yoxlayır."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """Kvadratın mövqeyini (koordinatlarını) geri qaytarır."""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Kvadratın mövqeyini təyin edir və yoxlayır."""
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """Kvadratın sahəsini hesablayır və qaytarır."""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Kvadratı '#' simvolu ilə çap edir, mövqeyini nəzərə alır."""
+        if self.__size == 0:
+            print("")
+            return
+
+        # Şaquli boşluq (position[1])
+        [print("") for i in range(self.__position[1])]
+
+        # Kvadratın sətirləri
+        for i in range(self.__size):
+            # Üfüqi boşluq (position[0])
+            print(" " * self.__position[0], end="")
+            # Kvadratın özü
+            print("#" * self.__size)
