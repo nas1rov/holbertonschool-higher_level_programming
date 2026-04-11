fa094a301351b68c181d4a9da0d95e7c432aa2ea
Kamran Nasirov <13778@holbertonstudents.com>
2026-04-11 12:18:10 +0000

    3-infinite_add.py

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    total = 0
+    # Iterate through sys.argv starting from index 1 to skip the script name
+    for arg in sys.argv[1:]:
+        total += int(arg)
+    
+    print("{}".format(total))
