555330eb10c9c1e92d84ca2a0108e367bbbccbf0
Kamran Nasirov <13778@holbertonstudents.com>
2026-04-11 13:01:02 +0000

    1-search_replace.py

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-def search_replace(my_list, search, replace):
-    # Siyahıdakı hər element üçün: əgər element search-ə bərabərdirsə replace qoy,
-    # deyilsə elementin özünü saxla.
-    return [replace if x == search else x for x in my_list]
+def print_list_integer(my_list=[]):
+    for i in my_list:
+        print("{:d}".format(i))
