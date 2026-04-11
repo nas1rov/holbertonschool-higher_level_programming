80bb54146527200371bb6f463da101f5154f98c6
Kamran Nasirov <13778@holbertonstudents.com>
2026-04-11 13:44:13 +0000

    6-square.py

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,90 +1,74 @@
 #!/usr/bin/python3
-"""
-Bu modul bir kvadratı təmsil edən Square klassını təqdim edir.
-Burada ölçü (size) və koordinat (position) idarəetməsi, həmçinin
-validasiya və vizual çap funksiyaları cəmlənmişdir.
-"""
+"""Singly linked list üçün Node və LinkedList klassları."""
 
 
-class Square:
-    """
-    Kvadratı təmsil edən və onun xüsusiyyətlərini idarə edən klass.
+class Node:
+    """Linked list-in düyününü (Node) təmsil edən klass."""
 
-    Attributes:
-        __size (int): Kvadratın tərəfinin ölçüsü.
-        __position (tuple): Kvadratın çap olunacağı (x, y) koordinatları.
-    """
-
-    def __init__(self, size=0, position=(0, 0)):
-        """
-        Yeni bir Square instansiyasını inisializasiya edir.
-
-        Args:
-            size (int): Kvadratın ölçüsü, susmaya görə 0-dır.
-            position (tuple): Kvadratın koordinatları, susmaya görə (0, 0)-dır.
-        """
-        self.size = size
-        self.position = position
+    def __init__(self, data, next_node=None):
+        """Node-u inisializasiya edir."""
+        self.data = data
+        self.next_node = next_node
 
     @property
-    def size(self):
-        """Kvadratın cari ölçüsünü (size) əldə etmək üçün istifadə olunur."""
-        return self.__size
+    def data(self):
+        """Data getter-i."""
+        return self.__data
 
-    @size.setter
-    def size(self, value):
-        """
-        Kvadratın ölçüsünü təyin edir və onun tam ədəd olmasını yoxlayır.
-
-        Args:
-            value (int): Kvadrat üçün yeni təyin ediləcək ölçü dəyəri.
-
-        Raises:
-            TypeError: Əgər daxil edilən dəyər tam ədəd (integer) deyilsə.
-            ValueError: Əgər daxil edilən dəyər 0-dan kiçikdirsə.
-        """
+    @data.setter
+    def data(self, value):
+        """Data setter-i (mütləq tam ədəd olmalıdır)."""
         if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = value
+            raise TypeError("data must be an integer")
+        self.__data = value
 
     @property
-    def position(self):
-        """Kvadratın koordinatlarını (position) əldə etmək üçün istifadə olunur."""
-        return self.__position
+    def next_node(self):
+        """Next_node getter-i."""
+        return self.__next_node
 
-    @position.setter
-    def position(self, value):
+    @next_node.setter
+    def next_node(self, value):
+        """Next_node setter-i (Node obyekti və ya None olmalıdır)."""
+        if not isinstance(value, Node) and value is not None:
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
+
+class SinglyLinkedList:
+    """Singly linked list-i idarə edən klass."""
+
+    def __init__(self):
+        """Boş bir list yaradır."""
+        self.__head = None
+
+    def __str__(self):
+        """List-i çap etmək üçün mətni formalaşdırır."""
+        values = []
+        tmp = self.__head
+        while tmp is not None:
+            values.append(str(tmp.data))
+            tmp = tmp.next_node
+        return "\n".join(values)
+
+    def sorted_insert(self, value):
         """
-        Kvadratın koordinatlarını təyin edir və onun doğruluğunu yoxlayır.
-
-        Args:
-            value (tuple): İki müsbət tam ədəddən ibarət tuple.
-
-        Raises:
-            TypeError: Əgər position formatı 2 müsbət tam ədəd olan tuple deyilsə.
-        """
-        if (not isinstance(value, tuple) or len(value) != 2 or
-                not all(isinstance(num, int) for num in value) or
-                not all(num >= 0 for num in value)):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
-
-    def area(self):
-        """
-        Kvadratın sahəsini (size kvadratını) hesablayır və qaytarır.
-
-        Returns:
-            int: Hesablanmış sahə dəyəri.
-        """
-        return self.__size ** 2
-
-    def my_print(self):
-        """
-        Kvadratı # simvolları ilə standart çıxışa (stdout) çap edir.
+        Yeni Node-u listin daxilinə artan sıra ilə əlavə edir.
         
-        Metod position[1] qədər şaquli boşluq (yeni sətir) və 
-        position[0] qədər üfüqi boşluq (space) buraxır.
-        Əgər size 0-dırsa, sadəcə boş bir sətir çap edir.
+        Args:
+            value (int): Əlavə olunacaq rəqəm.
         """
+        new_node = Node(value)
+        
+        # Əgər list boşdursa və ya yeni dəyər başdakından kiçikdirsə
+        if self.__head is None or self.__head.data >= value:
+            new_node.next_node = self.__head
+            self.__head = new_node
+        else:
+            # Düzgün mövqeyi tapmaq üçün listi gəzirik
+            tmp = self.__head
+            while tmp.next_node is not None and tmp.next_node.data < value:
+                tmp = tmp.next_node
+            
+            new_node.next_node = tmp.next_node
+            tmp.next_node = new_node
