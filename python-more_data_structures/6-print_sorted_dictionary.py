7d5611816b71721e4861f4ffdff746c003b3f5e4
Kamran Nasirov <13778@holbertonstudents.com>
2026-04-11 13:08:47 +0000

    6-print_sorted_dictionary.py

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for i in range(len(row)):
-            if i == len(row) - 1:
-                print("{:d}".format(row[i]), end="")
-            else:
-                print("{:d}".format(row[i]), end=" ")
-        print()
+def search_replace(my_list, search, replace):
+    return [replace if x == search else x for x in my_list]
