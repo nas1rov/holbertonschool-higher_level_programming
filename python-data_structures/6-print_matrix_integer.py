2af008a49b6a978b74635fc9593139f399e9513b
Kamran Nasirov <13778@holbertonstudents.com>
2026-04-11 12:38:07 +0000

    6-print_matrix_integer.py

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-def add_tuple(tuple_a=(), tuple_b=()):
-    # Extract values or use 0 if the tuple is too short
-    a1 = tuple_a[0] if len(tuple_a) >= 1 else 0
-    a2 = tuple_a[1] if len(tuple_a) >= 2 else 0
-    b1 = tuple_b[0] if len(tuple_b) >= 1 else 0
-    b2 = tuple_b[1] if len(tuple_b) >= 2 else 0
-    # Return a new tuple with the sums
-    return (a1 + b1, a2 + b2)
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i in range(len(row)):
+            if i < len(row) - 1:
+                print("{:d} ".format(row[i]), end="")
+            else:
+                print("{:d}".format(row[i]), end="")
+        print("")
