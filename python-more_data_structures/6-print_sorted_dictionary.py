a0ed817cd6e4c0d5d46e6f2f1d1a42d2ebf8a99c
Kamran Nasirov <13778@holbertonstudents.com>
2026-04-11 13:11:19 +0000

    6-print_sorted_dictionary.py

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-def search_replace(my_list, search, replace):
-    return [replace if x == search else x for x in my_list]
+def print_sorted_dictionary(a_dictionary):
+    for key in sorted(a_dictionary.keys()):
+        print("{}: {}".format(key, a_dictionary[key]))
