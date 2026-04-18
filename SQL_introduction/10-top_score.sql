a2ea978001cf0e9ba78e8eef147c067b7cfde992
Kamran Nasirov <13778@holbertonstudents.com>
2026-04-18 16:03:54 +0000

    10-top_score.sql

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- 'second_table' cədvəlindəki bütün yazıları score və name sütunları üzrə siyahılayır
+-- Nəticələr score sütununa görə azalan sıra ilə düzülür
+SELECT score, name FROM second_table ORDER BY score DESC;
