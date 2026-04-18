439293bda8818809469473a85205bf7bd428cbb2
Kamran Nasirov <13778@holbertonstudents.com>
2026-04-18 15:40:19 +0000

    1-safe_print_integer.py

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    """
-    Funksiya daxil olan dəyərin tam ədəd (integer) olub-olmadığını yoxlayır 
-    və onu formatlaşdırılmış şəkildə çap edir.
-    """
     try:
-        # Dəyəri integer formatında çap etməyə çalışırıq
         print("{:d}".format(value))
-        # Əgər çap uğurlu oldusa, True qaytarırıq
         return True
     except (ValueError, TypeError):
-        # Əgər formatlama zamanı xəta baş verərsə (məsələn, string gələrsə), 
-        # False qaytarırıq
         return False
